8ba9c56dac407ab37623c6d3b3c932f9fcf4bd70
Danny Delott <dannydelott@gmail.com>
2021-07-23 10:27:18 -0700

    Add tsc

---
+++ dist/hardhat.config.d.ts
@@ -0,0 +1,4 @@
+import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/config";
+declare const config: HardhatUserConfig;
+export default config;
---
+++ dist/hardhat.config.js
@@ -0,0 +1,48 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// uncomment this to compile vyper contracts
+// import "@nomiclabs/hardhat-vyper";
+require("@typechain/hardhat");
+const config = {
+    paths: {
+        sources: "src",
+    },
+    // uncomment this to compile vyper contracts.  the version in the file must match.
+    // vyper: {
+    // CRVPool
+    // version: "0.2.12",
+    // CRVToken
+    //   version: "0.2.8",
+    // },
+    solidity: {
+        compilers: [
+            {
+                version: "0.5.12",
+            },
+            {
+                version: "0.7.1",
+            },
+            {
+                version: "0.8.0",
+            },
+        ],
+    },
+    typechain: {
+        outDir: "types",
+        target: "ethers-v5",
+    },
+    networks: {
+        hardhat: {
+            gas: 1000000000000000000,
+            blockGasLimit: 0x1fffffffffffff,
+            allowUnlimitedContractSize: true,
+        },
+        mainnet: {
+            url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+        },
+        goerli: {
+            url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_GOERLI_API_KEY}`,
+        },
+    },
+};
+exports.default = config;
