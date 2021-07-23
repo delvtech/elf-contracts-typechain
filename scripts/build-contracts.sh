923549368e6d956f4b03c50ceca1a48ec0473797
Danny Delott <dannydelott@gmail.com>
2021-07-23 10:39:44 -0700

    Typechain has hard dependency on mocha?

---
+++ scripts/build-contracts.sh
@@ -0,0 +1,14 @@
+rm -rf artifacts types dist
+
+# Compile the contracts definitions and factory instances
+npx hardhat compile
+
+# Compile the factory definitions
+tsc --project tsconfig.json --outDir dist/ --declaration --emitDeclarationOnly
+
+# Copy over contract definitions
+cp types/* dist/types
+
+# copy over any pre-built local types as well
+cp -R src/localTypes/* dist/types
+cp -R src/localTypes/factories/* dist/types/factories
