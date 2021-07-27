22737d6f0a0d07244069c24fa087661fb1c2ed56
Danny Delott <dannydelott@gmail.com>
2021-07-26 18:17:11 -0700

    Separate tsc compilation from hardhat-typechain compilation in build step

--- dist/hardhat.config.d.ts
+++
@@ -1,4 +0,0 @@
-import "@typechain/hardhat";
-import { HardhatUserConfig } from "hardhat/config";
-declare const config: HardhatUserConfig;
-export default config;
--- scripts/build-contracts.sh
+++ scripts/build-contracts.sh
@@ -1,14 +1,23 @@
-rm -rf artifacts types dist
+rm -rf artifacts types dist compiled
 
-# Compile the contracts definitions and factory instances
+# Compile typechain to create contracts definitions and create factory
+# instances. Hardhat typechain will not make factory definitions, so we'll
+# compile the factory instances manually with tsc to create the defintions in
+# the next step.
 npx hardhat compile
 
-# Compile the factory definitions
-tsc --project tsconfig.json --outDir dist/ --declaration
+# Compile the factory definition files, including any local factories. Also
+# create the main index.d.ts definition file.
+tsc --project tsconfig.json
 
-# Copy over contract definitions
+mkdir -p dist/types/factories
+
+# Copy over contract definitions.
 cp types/* dist/types
 
-# copy over any pre-built local types as well
-cp -R src/localTypes/* dist/types
-cp -R src/localTypes/factories/* dist/types/factories
+# Copy over compiled factory definitions.
+cp compiled/types/factories/* dist/types/factories
+cp compiled/src/localTypes/factories/* dist/types/factories
+
+# Copy over the contracts index.d.ts
+cp compiled/types/index.d.ts dist/types/
