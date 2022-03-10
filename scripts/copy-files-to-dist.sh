992c2be0e6cc0f29107cb7e9c9bf6c5b7944a511
Danny Delott <dannydelott@gmail.com>
2022-03-10 14:33:34 -0800

    Move precompiled to top-level

--- scripts/copy-files-to-dist.sh
+++ scripts/copy-files-to-dist.sh
@@ -3,10 +3,10 @@ rm -rf dist
 mkdir -p dist
 
 # Copy over typecontracts.
-cp -R compiled/typechain-types/ dist/
+cp -R compiled/ dist/
 
 # Copy over the precompiled factories
-cp -R src/precompiled/factories/ dist/libraries/factories
+cp -R precompiled/factories/ dist/libraries/factories
 
 # Copy over the precompiled types 
-cp -R src/precompiled/*.d.ts dist/libraries
+cp -R precompiled/*.d.ts dist/libraries
