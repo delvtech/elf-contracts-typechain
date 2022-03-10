c05a63c5806f494ea7bf6c12d8993ad30d3b3991
Danny Delott <dannydelott@gmail.com>
2022-03-10 13:53:46 -0800

    Update scripts

--- scripts/copy-files-to-dist.sh
+++ scripts/copy-files-to-dist.sh
@@ -2,6 +2,11 @@ rm -rf dist
 
 mkdir -p dist
 
-# Copy over contract definitions.
-cp -R typechain-types/* dist/
-cp -R src/types/*.ts dist/libraries/
+# Copy over typecontracts.
+cp -R compiled/typechain-types/ dist/
+
+# Copy over the precompiled factories
+cp -R src/precompiled/factories/ dist/libraries/factories
+
+# Copy over the precompiled types 
+cp -R src/precompiled/*.d.ts dist/libraries
